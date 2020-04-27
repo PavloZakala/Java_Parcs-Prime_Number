437cc053f19c29792692eeff989d44b98ff8d2e7
ZPavlo <pzakala@gmail.com>
2020-04-27 22:16:54 +0300

    change MAX Range

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final int MAX_RANGE_SIZE = 64000;
+    private static final int MAX_RANGE_SIZE = 1000000;
 
     public static void main(String[] args) throws Exception {
 
